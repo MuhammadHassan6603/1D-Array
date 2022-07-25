8c9f2300b26a7af56e2746dcde3ef27d00447dde
Muhammad Hassan <105521572+MuhammadHassan6603@users.noreply.github.com>
2022-07-26 02:54:57 +0500

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,8 @@
+class Array1{
+    public static void main(String args[]){
+        int x[]={100,200,300,400,500};
+        for(int i=0;i<x.length;i++){
+            System.out.println(x[i]);
+        }
+    }
+}
---
+++ Cmd.java
@@ -0,0 +1,7 @@
+class Cmd{
+    public static void main(String arg[]){
+        System.out.println(arg[0]);
+        System.out.println(arg[1]);
+        System.out.println(arg[2]);
+    }
+}
